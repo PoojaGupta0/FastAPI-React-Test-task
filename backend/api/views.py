897d4ebe0987888076036b12c3ff202d5047cd06
Pooja Gupta <poojagupt5388@gmail.com>
2023-09-19 23:33:23 +0530

    Error handling in the api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,36 +1,62 @@
-import openai, os
+import os, json, openai
+
+from fastapi import HTTPException, Depends, Query
+from pydantic import BaseModel, ValidationError
 
 # Your OpenAI API key
 api_key = os.environ.get("open_ai_key")
 
 
+# Define a Pydantic model for query parameters
+class QueryParams(BaseModel):
+    country: str = Query(
+        ..., description="Country name is required", min_length=4, max_length=50
+    )
+    season: str = Query(
+        ..., description="Season is required", min_length=4, max_length=20
+    )
+
+
 class HomeView:
-    def __init__(self):
-        pass
+    # @handle_errors
+    def get(self, query_params: QueryParams = Depends()):
+        # Extract query parameters directly from the validated Pydantic model
+        country = query_params.country
+        season = query_params.season
 
-    def post(self, country, season):
+        # Check if the season is valid
+        valid_seasons = {"fall", "summer", "winter", "spring"}
+        if season.lower() not in valid_seasons:
+            raise HTTPException(status_code=400, detail="Season is invalid")
+
         # Define the prompt/question
-        prompt = f" Three Recommend activities for someone traveling to {country} in {season}."
+        prompt = f"Three recommended activities for someone traveling to {country} in {season}."
 
-        # Make a request to the OpenAI API
-        response = openai.Completion.create(
-            engine="text-davinci-002",  # Choose the appropriate engine
-            prompt=prompt,
-            max_tokens=50,  # Adjust the max tokens based on your desired response length
-            api_key=api_key,
-        )
+        try:
+            # Make a request to the OpenAI API
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=prompt,
+                max_tokens=50,
+                api_key=api_key,
+            )
 
-        # Extract and format the recommendations from the response
-        recommendations = response.choices[0].text.strip().replace("\n", "\n")
+            # Extract and format the recommendations from the response
+            recommendations = response.choices[0].text.strip().replace("\n", "\n")
 
-        return {
-            "season": season,
-            "country": country,
-            "recommendations": recommendations,
-        }
-
-    def get(self):
-        return "Welcome to Server is running"
+            return {
+                "season": season,
+                "country": country,
+                "recommendations": recommendations,
+            }
+        except SyntaxError as e:
+            raise HTTPException(status_code=400, detail=f"Syntax Error: {e}")
+        except ValueError as e:
+            raise HTTPException(status_code=400, detail=f"Value Error: {e}")
+        except ValidationError as e:
+            raise HTTPException(status_code=404, detail=f"Validation Error: {e}")
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
 
 
 home_view = HomeView()
