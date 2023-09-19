1907ddd3aba63a3df8d5741dee4e7c04613ebc6a
Pooja Gupta <poojagupt5388@gmail.com>
2023-09-19 23:33:23 +0530

    Error handling in the api

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -3,5 +3,4 @@ from api.views import home_view
 
 api_router = APIRouter()
 
-api_router.add_api_route("/travel/recommmendations", home_view.post, methods=["POST"])
-api_router.add_api_route("/", home_view.get, methods=["GET"])
+api_router.add_api_route("/travel/recomendations", home_view.get, methods=["GET"])
