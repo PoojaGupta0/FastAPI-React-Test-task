af35a26a79b222834e32bc6c273c3ecaf0c6b725
Pooja Gupta <poojagupt5388@gmail.com>
2023-09-21 21:34:55 +0530

    Optimized the code of the API and Modified the function of utils

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,12 @@
 import os, json, openai
 
 from fastapi import HTTPException, Depends, Query
-from pydantic import BaseModel, ValidationError, validator
+from pydantic import BaseModel, ValidationError
 from utils.utils import remove_numbers_bullet_points
+import requests.exceptions
 
 # Your OpenAI API key
-api_key = os.environ.get("open_ai_key")
+openai_api_key = os.environ.get("open_ai_key")
 
 
 # Define a Pydantic model for query parameters
@@ -17,42 +18,52 @@ class QueryParams(BaseModel):
         ..., description="Season is required", min_length=4, max_length=20
     )
 
-
 class HomeView:
-    # @handle_errors
-    def get(self, query_params: QueryParams = Depends()):
-        # Extract query parameters directly from the validated Pydantic model
-        country = query_params.country
-        season = query_params.season
-
-        # Define the prompt/question
+    def get_recommendations(self, country, season):
+            # Define the prompt/question
         prompt = f"Three recommended activities for someone traveling to {country} in {season}."
 
+        # Make a request to the OpenAI API
         try:
-            # Make a request to the OpenAI API
             response = openai.Completion.create(
                 engine="text-davinci-002",
                 prompt=prompt,
                 max_tokens=50,
-                api_key=api_key,
+                api_key=openai_api_key,  # Use environment variable
             )
 
             # Extract and format the recommendations from the response
             recommendations = response.choices[0].text.strip().replace("\n", "\n")
-            # recommend = remove_numbers_bullet_points(recommendations)
+            return recommendations
+
+        except openai.error.RateLimitError as e:
+            raise HTTPException(status_code=429, detail=f"Rate limit exceeded. Retry after 3 seconds")
+
+        except requests.exceptions.Timeout:
+            raise HTTPException(status_code=504, detail=f"Request to OpenAI API timed out. Please try again later.")
+
+        except openai.error.OpenAIError as e:
+            raise HTTPException(status_code=400, detail=str(e))
+
+    def get(self, query_params: QueryParams = Depends()):
+        country = query_params.country
+        season = query_params.season
+
+        try:
+            recommendations = self.get_recommendations(country, season)
+            recommendations = remove_numbers_bullet_points(recommendations)
+
             return {
                 "season": season,
-                "country": country,
-                "recommendations": recommendations,
+                "country": country.title(),
+                "recommendations": recommendations
             }
-        except SyntaxError as e:
-            raise HTTPException(status_code=400, detail=f"Syntax Error: {e}")
-        except ValueError as e:
-            raise HTTPException(status_code=400, detail=f"Value Error: {e}")
-        except ValidationError as e:
-            raise HTTPException(status_code=404, detail=f"Validation Error: {e}")
+
+        except (SyntaxError, ValueError, ValidationError) as e:
+            raise HTTPException(status_code=400, detail=str(e))
+
         except Exception as e:
-            raise HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
+            raise HTTPException(status_code=500, detail=str(e))
 
-
+# Instantiate HomeView class
 home_view = HomeView()
--- backend/utils/utils.py
+++ backend/utils/utils.py
@@ -1,10 +1,18 @@
 import re
 
+# Custom function to clean up and format a single line
+def clean_up_line(line):
+    # Remove leading numbers, bullets, and whitespace
+    cleaned_line = re.sub(r'^\s*[0-9]+[.)]?\s*•?\s*-?\s*', '', line)
+    # Add a full stop at the end if it's not there
+    if cleaned_line and not cleaned_line.endswith('.'):
+        cleaned_line += '.'
+    return cleaned_line.strip()
+
 def remove_numbers_bullet_points(recommendations):
-    # Split the recommendations into lines
+    # Split the string using a regular expression to match numbering and bullets
     lines = recommendations.split('\n')
 
-    # Join the lines back into a single string without numbers or bullet points
-    cleaned_recommendations = '\n'.join(lines)
-
-    return cleaned_recommendations
+    # Clean up and filter the lines
+    elements = [clean_up_line(line) for line in lines if line.strip()]
+    return elements
